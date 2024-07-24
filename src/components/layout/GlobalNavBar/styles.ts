2340d9350065037097e50c9c8ace0fdcb87ffe78
kayeon0805 <kayeon8855@gmail.com>
2024-07-24 20:03:16 +0900

    GNB minor fix

--- src/components/layout/GlobalNavBar/styles.ts
+++ src/components/layout/GlobalNavBar/styles.ts
@@ -7,7 +7,7 @@ export const NavBar = styled.nav`
   display: flex;
   flex-direction: column;
   align-items: center;
-  gap: 20px;
+  gap: 30px;
   width: var(--nav-width);
   height: 100dvh;
   min-height: 500px;
@@ -43,10 +43,12 @@ export const NavList = styled.ul`
   display: flex;
   flex-direction: column;
   align-items: center;
-  gap: 30px;
-  width: fit-content;
+  gap: 40px;
+  width: 100%;
 
   li {
+    width: 100%;
+    height: 40px;
     &.active svg path {
       fill: #ffb929;
     }
